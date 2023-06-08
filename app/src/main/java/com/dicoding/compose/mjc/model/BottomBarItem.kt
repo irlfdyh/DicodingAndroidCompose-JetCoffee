4d789d76e13bec54172b48285c63495f2ab6877a
Irshal Firdeansyah <irshalfir@gmail.com>
2023-06-08 09:14:59 +0700

    add bottom navigation bar

---
+++ app/src/main/java/com/dicoding/compose/mjc/model/BottomBarItem.kt
@@ -0,0 +1,8 @@
+package com.dicoding.compose.mjc.model
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomBarItem(
+    val title: String,
+    val icon: ImageVector
+)
--- app/src/main/java/com/dicoding/compose/mjc/ui/MainActivity.kt
+++ app/src/main/java/com/dicoding/compose/mjc/ui/MainActivity.kt
@@ -9,11 +9,21 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -23,6 +33,7 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dicoding.compose.mjc.R
+import com.dicoding.compose.mjc.model.BottomBarItem
 import com.dicoding.compose.mjc.model.Menu
 import com.dicoding.compose.mjc.model.dummyBestSellerMenu
 import com.dicoding.compose.mjc.model.dummyCategory
@@ -46,22 +57,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
-    Column(
-        modifier = Modifier.verticalScroll(rememberScrollState())
-    ) {
-        Banner()
-        HomeSection(
-            title = stringResource(R.string.section_category),
-            content = { CategoryRow() }
-        )
-        HomeSection(
-            title = stringResource(id = R.string.section_favorite_menu),
-            content = { MenuRow(menus = dummyMenu) }
-        )
-        HomeSection(
-            title = stringResource(id = R.string.section_best_seller_menu),
-            content = { MenuRow(menus = dummyBestSellerMenu) }
-        )
+    Scaffold(
+        bottomBar = { BottomBar() }
+    ) { contentPadding ->
+        Column(
+            modifier = Modifier
+                .padding(contentPadding)
+                .verticalScroll(rememberScrollState())
+        ) {
+            Banner()
+            HomeSection(
+                title = stringResource(R.string.section_category),
+                content = { CategoryRow() }
+            )
+            HomeSection(
+                title = stringResource(id = R.string.section_favorite_menu),
+                content = { MenuRow(menus = dummyMenu) }
+            )
+            HomeSection(
+                title = stringResource(id = R.string.section_best_seller_menu),
+                content = { MenuRow(menus = dummyBestSellerMenu) }
+            )
+        }
     }
 }
 
@@ -127,6 +144,47 @@ fun HomeSection(
         content()
     }
 }
+
+@Composable
+fun BottomBar(
+    modifier: Modifier = Modifier
+) {
+    NavigationBar(
+        modifier = modifier,
+        containerColor = MaterialTheme.colorScheme.background
+    ) {
+        val navigationItems = listOf(
+            BottomBarItem(
+                title = stringResource(R.string.menu_home),
+                icon = Icons.Default.Home
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_favorite),
+                icon = Icons.Default.Favorite
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_profile),
+                icon = Icons.Default.AccountCircle
+            ),
+        )
+        navigationItems.map {
+            NavigationBarItem(
+                selected = it.title == navigationItems[0].title,
+                onClick = { },
+                label = {
+                    Text(it.title)
+                },
+                icon = {
+                    Icon(
+                        imageVector = it.icon,
+                        contentDescription = it.title
+                    )
+                }
+            )
+        }
+    }
+}
+
 
 @Preview(showBackground = true, device = Devices.PIXEL_4)
 @Composable
