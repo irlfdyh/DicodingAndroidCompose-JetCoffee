68c7bbea4d3cea51c6d6934e7d800d261a227019
Irshal Firdeansyah <irshalfir@gmail.com>
2023-06-08 07:17:00 +0700

    add banner component

--- app/src/main/java/com/dicoding/compose/mjc/ui/MainActivity.kt
+++ app/src/main/java/com/dicoding/compose/mjc/ui/MainActivity.kt
@@ -3,9 +3,20 @@ package com.dicoding.compose.mjc.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dicoding.compose.mjc.R
+import com.dicoding.compose.mjc.ui.components.Search
 import com.dicoding.compose.mjc.ui.theme.MyJetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +32,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
+    Column {
+        Banner()
+    }
+}
 
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        Search()
+    }
 }
 
 @Preview(showBackground = true, device = Devices.PIXEL_4)
