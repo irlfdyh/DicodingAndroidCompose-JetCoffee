1b1fb34f2b703f68c2050965ed1d01882f0c23f3
Irshal Firdeansyah <irshalfir@gmail.com>
2023-06-08 07:46:54 +0700

    add menu section

--- app/src/main/java/com/dicoding/compose/mjc/ui/MainActivity.kt
+++ app/src/main/java/com/dicoding/compose/mjc/ui/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -21,8 +23,12 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dicoding.compose.mjc.R
+import com.dicoding.compose.mjc.model.Menu
+import com.dicoding.compose.mjc.model.dummyBestSellerMenu
 import com.dicoding.compose.mjc.model.dummyCategory
+import com.dicoding.compose.mjc.model.dummyMenu
 import com.dicoding.compose.mjc.ui.components.CategoryItem
+import com.dicoding.compose.mjc.ui.components.MenuItem
 import com.dicoding.compose.mjc.ui.components.Search
 import com.dicoding.compose.mjc.ui.components.SectionText
 import com.dicoding.compose.mjc.ui.theme.MyJetCoffeeTheme
@@ -40,10 +46,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
-    Column {
+    Column(
+        modifier = Modifier.verticalScroll(rememberScrollState())
+    ) {
         Banner()
         SectionText(title = stringResource(R.string.section_category))
         CategoryRow()
+        SectionText(title = stringResource(id = R.string.section_favorite_menu))
+        MenuRow(menus = dummyMenu)
+        SectionText(title = stringResource(id = R.string.section_best_seller_menu))
+        MenuRow(menus = dummyBestSellerMenu)
     }
 }
 
@@ -76,6 +88,23 @@ fun CategoryRow(
         }
     }
 }
+
+@Composable
+fun MenuRow(
+    modifier: Modifier = Modifier,
+    menus: List<Menu>
+) {
+    LazyRow(
+        modifier = modifier,
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(menus, key = { it.title }) { menu ->
+            MenuItem(menu = menu)
+        }
+    }
+}
+
 
 @Preview(showBackground = true, device = Devices.PIXEL_4)
 @Composable
---
+++ app/src/main/java/com/dicoding/compose/mjc/ui/components/MenuItem.kt
@@ -0,0 +1,75 @@
+package com.dicoding.compose.mjc.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dicoding.compose.mjc.R
+import com.dicoding.compose.mjc.model.Menu
+
+@Composable
+fun MenuItem(
+    modifier: Modifier = Modifier,
+    menu: Menu
+) {
+    Card(
+        modifier = modifier.width(140.dp),
+        shape = RoundedCornerShape(8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.background
+        )
+    ) {
+        Column {
+            Image(
+                painter = painterResource(id = menu.image),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(120.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            )
+            Column(modifier = Modifier.padding(8.dp)) {
+                Text(
+                    text = menu.title,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
+                Text(
+                    text = menu.price,
+                    style = MaterialTheme.typography.titleSmall
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun MenuItemPreview() {
+    MaterialTheme {
+         MenuItem(
+             modifier = Modifier.padding(8.dp),
+             menu = Menu(R.drawable.menu2, "Hot Pumpkin Spice Latte Premium", "Rp 18.000")
+         )
+    }
+}
